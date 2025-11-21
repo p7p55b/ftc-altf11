7c1c503d46d712147c036443ffec42b098a1bb99
Foxter <155650056+p7p55b@users.noreply.github.com>
2025-11-21 13:48:56 +0100

    Add files via upload

--- main-2.java
+++ main-2.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import java.util.concurrent.TimeUnit;
 
 
-@TeleOp(name = "manual_final_v0.0.4")
+@TeleOp(name = "manual_final_v0.0.5")
 public class main-2 extends LinearOpMode {
 
 // setup les noms des moteurs
@@ -32,37 +32,50 @@ public class main-2 extends LinearOpMode {
     if (opModeIsActive()) {
       while (opModeIsActive()) {
 
-// vérifie qu'on se soit pas en mode triangle
+// verifie qu'on se soit pas en mode triangle
         while (sht == 0) {
 
 // reset la vitesse des moteurs a 0
           dcom0.setPower(0);
           dcom1.setPower(0);
 
-// si le stick est a gauche, met le moteur droit à avancer, et le gauche a reculer
+// si le stick est a gauche, a met le moteur droit à avancer, et le gauche a reculer
 	// il met la vitesse proportionellement a l'inclinaison du stick, et la presssion sur le trigger
           if (gamepad1.left_stick_x < -0.001) {
             dcom0.setPower(gamepad1.right_trigger * gamepad1.left_stick_x);
             dcom1.setPower(-1 * gamepad1.right_trigger * gamepad1.left_stick_x);
           }
-          if (gamepad1.left_stick_x > -0.001) {
+
+// si le stick est a droite; met le moteur droit à reculer, et le gauche a avancer
+          if (gamepad1.left_stick_x > 0.001) {
             dcom0.setPower(gamepad1.left_stick_x * gamepad1.right_trigger);
             dcom1.setPower(-(gamepad1.right_trigger * gamepad1.left_stick_x));
           }
+
+// si le stick est neutre;
+
           if (gamepad1.left_stick_x == 0) {
-            dcom0.setPower(-(gamepad1.left_trigger));
-            dcom1.setPower(-(gamepad1.left_trigger));
-          } else {
-            dcom0.setPower(gamepad1.right_trigger);
-            dcom1.setPower(gamepad1.right_trigger);
-          }
-             if (gamepad1.y) {
+
+// si le trigger gauche est presser, il brake
+	     if (gamepad1.left_trigger > 0) {
+                dcom0.setPower(-(gamepad1.left_trigger));
+           	dcom1.setPower(-(gamepad1.left_trigger));
+
+// si le stick est neutre; avance
+	     } else {
+		dcom0.setPower(gamepad1.right_trigger);
+		dcom1.setPower(gamepad1.right_trigger);
+             }
+
+          if (gamepad1.y) {
               sht = 1;
               break;
             }
+
           telemetry.addData("key", 123);
           telemetry.update();
           }
+
           if (gamepad1.x) {
             dcom2.setPower(1);
           } else {
