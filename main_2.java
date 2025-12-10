75f1bc27ad369c501a321c42a0c8eac264bbba5f
Foxter <155650056+p7p55b@users.noreply.github.com>
2025-12-10 12:04:09 +0000

    bla bla bla

---
+++ main_2.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import java.util.concurrent.TimeUnit;
+
+
+@TeleOp(name = "manual_final_v0.1.7")
+public class main_2 extends LinearOpMode {
+  private DcMotor dcom0;
+  private DcMotor dcom1;
+  private DcMotor dcom2;
+  @Override
+  public void runOpMode() {
+    dcom0 = hardwareMap.get(DcMotor.class, "dcom0");
+    dcom1 = hardwareMap.get(DcMotor.class, "dcom1");
+    dcom2 = hardwareMap.get(DcMotor.class, "dcom2");
+    waitForStart();
+    int sht = 0;
+    if (opModeIsActive()) {
+      while (opModeIsActive()) {
+        while (sht == 0) {
+          dcom0.setPower(0);
+          dcom1.setPower(0);
+          if (gamepad1.left_stick_x < -0.001) {
+            dcom0.setPower(-(gamepad1.right_trigger * gamepad1.left_stick_x));
+            dcom1.setPower(-(gamepad1.right_trigger * gamepad1.left_stick_x));
+          }
+          if (gamepad1.left_stick_x > 0.001) {
+            dcom0.setPower(-(gamepad1.left_stick_x * gamepad1.right_trigger));
+            dcom1.setPower(-(gamepad1.right_trigger * gamepad1.left_stick_x));
+          }
+          if (gamepad1.left_stick_x == 0) {
+              if (gamepad1.right_trigger > 0) {
+                dcom0.setPower(-(gamepad1.right_trigger));
+                dcom1.setPower(gamepad1.right_trigger);
+            }
+            else {
+              dcom0.setPower(gamepad1.left_trigger);
+              dcom1.setPower(-(gamepad1.left_trigger));
+            }
+          }
+          if (gamepad1.y) {
+            sht = 1;
+            break;
+          }
+          telemetry.addData("key", 123);
+          telemetry.update();
+        }
+
+        while (gamepad1.x) {
+          dcom2.setPower(1);
+        dcom2.setPower(0);
+        }
+
+        if (gamepad1.y) {
+            sht = 0;
+        }
+      }
+    }
+  }
+}
+
