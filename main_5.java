998317283ebb63d2d11c75e85820a93adc0d8840
Foxter <155650056+p7p55b@users.noreply.github.com>
2025-12-10 14:01:23 +0000

    Fix fix fix of the feature

--- main_5.java
+++ main_5.java
@@ -5,10 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import java.util.concurrent.TimeUnit;
 
-
 @TeleOp(name = "manual_final_v0.1.7")
 public class main_5 extends LinearOpMode {
-	//set les moteurs
   private DcMotor dcom0;
   private DcMotor dcom1;
   private DcMotor dcom2;
@@ -26,40 +24,41 @@ public class main_5 extends LinearOpMode {
         while (sht == 0) {
           dcom0.setPower(0);
           dcom1.setPower(0);
-		  //tourner a gauche
           if (gamepad1.left_stick_x < -0.001) { 
             dcom0.setPower(-(gamepad1.right_trigger * gamepad1.left_stick_x));
             dcom1.setPower(-(gamepad1.right_trigger * gamepad1.left_stick_x));
           }
-		  //tourner a droite
           if (gamepad1.left_stick_x > 0.001) {
             dcom0.setPower(-(gamepad1.left_stick_x * gamepad1.right_trigger));
             dcom1.setPower(-(gamepad1.right_trigger * gamepad1.left_stick_x));
           }
-			//avancer
           if (gamepad1.left_stick_x == 0) {
               if (gamepad1.right_trigger > 0) {
                 dcom0.setPower(-(gamepad1.right_trigger));
                 dcom1.setPower(gamepad1.right_trigger);
             }
-			//reculer
             else {
               dcom0.setPower(gamepad1.left_trigger);
               dcom1.setPower(-(gamepad1.left_trigger));
             }
           }
-		      //mode tir grace triangle
-          if (gamepad1.y) {
+          if (gamepad1.x) {
             sht = 1;
+            break;
           }
           telemetry.addData("key", 123);
           telemetry.update();
         }
-        while (sht == 1){
-          dcom3.setPower(1);
-          if (gamepad1.y) {
+        dcom0.setPower(0);
+        dcom1.setPower(0);
+        dcom2.setPower(0);
+        dcom3.setPower(0);
+        while (sht == 1) {
+          dcom2.setPower(1);
+          if (gamepad1.x) {
             sht = 0;
-	          dcom2.setPower(0);
+            dcom2.setPower(0);
+            break;
           }
         }
       }
